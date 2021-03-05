2278b4fd1edfe42192dfc32ec372f8758d965896
Mabuza98 <70321095+Mabuza98@users.noreply.github.com>
2021-03-05 03:39:27 +0200

    Add project files.

---
+++ EcoSplashScreen/EcoSplashScreen.Android/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Drawing;
+
+namespace EcoSplashScreen
+{
+    [Activity(Label = "EcoSplashScreen", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MainTheme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+
+            base.OnCreate(bundle);
+
+            //Show Splash Screen
+            var config = new EcoSplashScreen(this)
+                .WithFullScreen()
+                .WithTargetActivity(Java.Lang.Class.FromType(typeof(TargetActivity)))
+                .WithSplashTimeOut(5000) // 5 sec                
+                .WithBackgroundColor(Color.LawnGreen)
+                .WithHeaderText("Welcome Guest !!!")
+                .WithFooterText("Copyright 2021")
+                .WithBeforLogoText("EcoBloc")
+                .WithAfterLogoText("welcome to EcoBloc");
+
+
+                // text color
+            object p = config.HeaderTextView.SetTextColor(Color.Green);
+            config.FooterTextView.SetTextColor(Color.Green);
+            config.BeforeLogoTextView.SetTextColor(Color.Green);
+            config.AfterLogoTextView.SetTextColor(Color.Green);
+
+            // view
+            View view = config.Create();
+
+            // Content View
+            SetContentView(view);
+        }
+
+    }
+}
---
+++ EcoSplashScreen/EcoSplashScreen/TargetActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.TizenSpecific;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+namespace EcoSplashScreen
+{
+    [Activity(Label = "TargetActivity",  Theme = "@style/MainTheme.AppCompat.Light.NoActionBar")]
+    public class TargetActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Target);
+        }
+    }
+}
